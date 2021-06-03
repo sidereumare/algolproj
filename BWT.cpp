b5ede0eda22689694a5b3f6f8a4aeb96e36d0121
Kim Kyungmin <qaws2051@gmail.com>
2021-06-04 01:50:52 +0900

    update makeBWT

--- BWT.cpp
+++ BWT.cpp
@@ -1,9 +1,21 @@
 #include "BWT.h"
+#include <algorithm>
+void BWT::Restore(const vector<string>& ShortLeads, const string& ref, const vector<int>& snipPos) {
 
-void BWT::Restore(const vector<string>& ShortLeads, const string& ref, const vector<int>& snipPos)
-{
 }
 
 void BWT :: makeBWT(const string& ref){
+  int length = ref.length();
+  string origin = ref + "$";
+  string double_origin = origin+origin;
 
+  vector<string> fillTheRest;
+  fillTheRest.push_back(origin);
+  for(int i = 1 ; i < length; i++) {
+    fillTheRest.push_back(double_origin.substr(length-i-1, length));
+  }
+  for(int i = 0 ; i < length; i++) {
+    bwt += fillTheRest[i][length-1];
+  }
+  sort(bwt.begin(), bwt.end());
 }
